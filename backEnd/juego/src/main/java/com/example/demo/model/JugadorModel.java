d9ef96908788cf04a4a00fcf7a19fb93066e7e81
Paola Casadiegos Vaca <paolacasadiegos@wposs.com>
2022-05-13 07:27:17 -0500

    primer commit

---
+++ backEnd/juego/src/main/java/com/example/demo/model/JugadorModel.java
@@ -0,0 +1,54 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
+public class JugadorModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idjugador;
+	private String Usuario;
+	private Integer premio;
+	
+	
+	public JugadorModel(String usuario, Integer premio) {
+		super();
+		Usuario = usuario;
+		this.premio = premio;
+	}
+	
+	public Integer getIdjugador() {
+		return idjugador;
+	}
+
+	public void setIdjugador(Integer idjugador) {
+		this.idjugador = idjugador;
+	}
+
+	public String getUsuario() {
+		return Usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		Usuario = usuario;
+	}
+
+	public Integer getPremio() {
+		return premio;
+	}
+
+	public void setPremio(Integer premio) {
+		this.premio = premio;
+	}
+
+	public JugadorModel() {
+		super();
+	}
+	
+	
+	
+	
+}
