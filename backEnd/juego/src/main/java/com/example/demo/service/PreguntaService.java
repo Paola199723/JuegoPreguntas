e098ea162dd2fdd76bfc4ad8ecd73d67317d41b8
Paola Casadiegos Vaca <paolacasadiegos@wposs.com>
2022-05-13 11:44:17 -0500

    IMPLEMENTACION-> Se crea el servicio para listar las preguntas

---
+++ backEnd/juego/src/main/java/com/example/demo/service/PreguntaService.java
@@ -0,0 +1,23 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Preguntas;
+import com.example.demo.repository.PreguntasRepository;
+
+@Service
+public class PreguntaService {
+	@Autowired
+	PreguntasRepository repositorio;
+	
+	public List<Preguntas> listarPreguntas(){
+		return repositorio.findAll();
+	}
+	
+	
+	
+	
+}
